ea5b5a74f51ee03c31c51b7644f920c0ccb5e97d
MrGadget84 <superhyper52@gmail.com>
2026-03-28 19:18:06 +0400

    Update gcc2-4.9.3.sh

--- toolchains/gcc2-4.9.3.sh
+++ toolchains/gcc2-4.9.3.sh
@@ -1,45 +1,29 @@
 #!/bin/bash
-# install_gcc_4.9.3.sh – установка кросс-компилятора GCC 4.9.3 для ARM (cortex-a9)
-# Основано на toolchain от FriendlyARM [citation:3][citation:4]
+# GCC-4.9.3.sh - установка кросс-компилятора arm-eabi-gcc 4.9.3
 
-set -e  # прерывать скрипт при любой ошибке
+set -e
 
-TOOLCHAIN_URL="https://github.com/friendlyarm/prebuilts/raw/master/gcc-x64/arm-cortexa9-linux-gnueabihf-4.9.3.tar.xz"
-INSTALL_DIR="/opt/FriendlyARM/toolchain/4.9.3"
-TARBALL="arm-cortexa9-linux-gnueabihf-4.9.3.tar.xz"
+INSTALL_DIR="/opt/gcc-4.9.3"
+TOOLCHAIN_URL="https://releases.linaro.org/components/toolchain/binaries/4.9-2017.01/arm-linux-gnueabihf/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabihf.tar.xz"
+TARBALL="gcc-4.9.3.tar.xz"
 
-echo "=== Установка ARM toolchain GCC 4.9.3 для сборки ядра s3ve3g ==="
-
-# Проверка: система должна быть 64-битной Linux
-if [ "$(uname -m)" != "x86_64" ]; then
-    echo "Ошибка: этот toolchain предназначен для 64-битных систем x86_64."
-    exit 1
-fi
-
-# Создаём директорию для установки (требуются права sudo)
-echo "Создаю директорию $INSTALL_DIR (требуется sudo)..."
 sudo mkdir -p "$INSTALL_DIR"
 
-# Скачиваем архив, если его нет
 if [ ! -f "$TARBALL" ]; then
-    echo "Скачиваю toolchain..."
     wget -O "$TARBALL" "$TOOLCHAIN_URL"
-else
-    echo "Архив $TARBALL уже есть, пропускаю загрузку."
 fi
 
-# Распаковываем в целевую директорию
-echo "Распаковываю в $INSTALL_DIR (требуется sudo)..."
-sudo tar xf "$TARBALL" -C "$INSTALL_DIR" --strip-components=1
+sudo tar -xf "$TARBALL" -C "$INSTALL_DIR" --strip-components=1
 
-# Настраиваем PATH для текущей сессии
-export PATH="$INSTALL_DIR/bin:$PATH"
+sudo ln -sf "$INSTALL_DIR/bin/arm-linux-gnueabihf-gcc" "$INSTALL_DIR/bin/arm-eabi-gcc"
+sudo ln -sf "$INSTALL_DIR/bin/arm-linux-gnueabihf-g++" "$INSTALL_DIR/bin/arm-eabi-g++"
+sudo ln -sf "$INSTALL_DIR/bin/arm-linux-gnueabihf-ld" "$INSTALL_DIR/bin/arm-eabi-ld"
+sudo ln -sf "$INSTALL_DIR/bin/arm-linux-gnueabihf-ar" "$INSTALL_DIR/bin/arm-eabi-ar"
+
 echo "export PATH=\"$INSTALL_DIR/bin:\$PATH\"" >> ~/.bashrc
-echo "export GCC_COLORS=auto" >> ~/.bashrc
+export PATH="$INSTALL_DIR/bin:$PATH"
 
-# Очистка
 rm -f "$TARBALL"
 
-echo "=== Установка завершена ==="
-echo "Проверьте версию компилятора, выполнив: arm-linux-gcc --version"
-echo "Чтобы изменения PATH вступили в силу в текущем окне, выполните: source ~/.bashrc"
+echo "Установлено. Проверка:"
+arm-eabi-gcc --version
