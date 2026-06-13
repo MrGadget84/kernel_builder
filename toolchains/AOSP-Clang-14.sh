6b10d40b8f3bc14fe042d264d85beb92a20f8c61
MrGadget84 <superhyper52@gmail.com>
2026-06-13 22:19:39 +0400

    fix

--- toolchains/AOSP-Clang-14.sh
+++ toolchains/AOSP-Clang-14.sh
@@ -23,7 +23,8 @@ case $1 in
 
   "build" )
     export PATH="$clang/bin:$gcc64/bin:$gcc/bin:/usr/bin:${PATH}"
-    make -j$NJOBS O=out CC=clang LD=ld.lld ARCH=arm64 SUBARCH=arm64 $2
+    # make -j$NJOBS O=out CC=clang LD=ld.lld ARCH=arm64 SUBARCH=arm64 $2
+    make O=out ARCH=arm64 $2
     make -j$NJOBS O=out \
       CROSS_COMPILE="aarch64-linux-android-" \
       CROSS_COMPILE_ARM32="arm-linux-androideabi-" \
