c08342a3996a019e394c82f128bc47bfca37477c
MrGadget84 <superhyper52@gmail.com>
2026-06-14 20:04:23 +0400

    Update shattered-Clang-15.sh

--- toolchains/shattered-Clang-15.sh
+++ toolchains/shattered-Clang-15.sh
@@ -36,6 +36,7 @@ case $1 in
       HOSTCXX=clang++ \
       HOSTLD=ld.lld \
       HOSTAR=llvm-ar \
+      KCFLAGS="-Wno-int-conversion -Wno-unused-variable" \
       2>&1 | tee ${CUR_TOOLCHAIN}.log
     sh ${outside}/ver_toolchain.sh clang ld.lld > ${CUR_TOOLCHAIN}.info
   ;;
