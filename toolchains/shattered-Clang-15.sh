2bb8f41a9c07fb3d92da843da4d7b4cbb7567345
MrGadget84 <superhyper52@gmail.com>
2026-06-15 10:24:48 +0400

    Update shattered-Clang-15.sh

--- toolchains/shattered-Clang-15.sh
+++ toolchains/shattered-Clang-15.sh
@@ -14,15 +14,20 @@ case $1 in
       tar -zxf a.tar.gz
     fi
   ;;
-
+  
   "build" )
-    export PATH="${dir}/bin:/usr/bin:${PATH}"
+    export PATH="$clang/bin:$gcc64/bin:$gcc/bin:/usr/bin:${PATH}"
+    sed -i 's/info BTF/return 0/g' scripts/link-vmlinux.sh 2>/dev/null || :
+    sed -i 's/cmd btf/return 0/g' scripts/link-vmlinux.sh 2>/dev/null || :
     git submodule update --init --recursive
-    make -j$NJOBS O=out CC=clang LD=ld.lld ARCH=arm64 SUBARCH=arm64 $2
+    make O=out ARCH=arm64 a32_lineage_defconfig
+    make O=out ARCH=arm64 olddefconfig
     make -j$NJOBS O=out \
-      CROSS_COMPILE="aarch64-linux-gnu-" \
-      CROSS_COMPILE_ARM32="arm-linux-gnueabi-" \
-      CROSS_COMPILE_COMPAT="arm-linux-gnueabi-" \
+      CROSS_COMPILE="aarch64-linux-android-" \
+      CROSS_COMPILE_ARM32="arm-linux-androideabi-" \
+      CROSS_COMPILE_COMPAT="arm-linux-androideabi-" \
+      CLANG_TRIPLE="aarch64-linux-gnu-" \
+      LD_LIBRARY_PATH="$clang/lib64:$LD_LIBRABRY_PATH" \
       CC="clang -w" \
       LD=ld.lld \
       NM=llvm-nm \
@@ -40,4 +45,3 @@ case $1 in
       2>&1 | tee ${CUR_TOOLCHAIN}.log
     sh ${outside}/ver_toolchain.sh clang ld.lld > ${CUR_TOOLCHAIN}.info
   ;;
-esac
