72c4c031d7e06f9e5639613264167d7abf6ea960
MrGadget84 <superhyper52@gmail.com>
2026-08-15 20:28:45 +0400

    fix

--- ksu/sukisu/sukisu.sh
+++ ksu/sukisu/sukisu.sh
@@ -13,16 +13,7 @@ KSU_git_ver=$(cd $SUKI_DIR && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 10000 + 200))
 
 patchesdir="$outside/ksu/sukisu/hooks/"
-#suspatchesdir="$outside/ksu/sukisu/sus/"
-
-if [[ -d "$patchesdir" ]]; then
-  for patch_file in "$patchesdir"/*.patch ; do
-    patch -p1 < "$patch_file"
-  done
-else
-  echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
-  exit 1
-fi
+suspatchesdir="$outside/ksu/sukisu/sus/"
 
 if [[ -d "$suspatchesdir" ]]; then
   for patch_file in "$suspatchesdir"/*.patch ; do
@@ -30,6 +21,15 @@ if [[ -d "$suspatchesdir" ]]; then
   done
 else
   echo "patching susfs failed, the kernel version you want to patch doesnt have patches here yet"
+  exit 1
+fi
+
+if [[ -d "$patchesdir" ]]; then
+  for patch_file in "$patchesdir"/*.patch ; do
+    patch -p1 < "$patch_file"
+  done
+else
+  echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
   exit 1
 fi
 
