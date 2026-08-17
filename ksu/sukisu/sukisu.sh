70a140f3e472e5f468537a6961f6d1db18c23ca1
MrGadget84 <superhyper52@gmail.com>
2026-08-17 15:22:32 +0300

    Update sukisu.sh

--- ksu/sukisu/sukisu.sh
+++ ksu/sukisu/sukisu.sh
@@ -10,14 +10,14 @@ curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup
 git add . && git commit -am "drivers: KernelSU"
 SUKI_DIR="drivers/kernelsu"
 KSU_git_ver=$(cd $SUKI_DIR && git rev-list --count HEAD)
-KSU_ver=$KSU_git_ver
+KSU_ver=$(($KSU_git_ver + 10000 + 200))
 
 patchesdir="$outside/ksu/sukisu/hooks/"
-# suspatchesdir="$outside/ksu/sukisu/sus/"
+#suspatchesdir="$outside/ksu/sukisu/sus/"
 
 #if [[ -d "$suspatchesdir" ]]; then
 #  for patch_file in "$suspatchesdir"/*.patch ; do
-#    patch -p1 --fuzz=3 --ignore-whitespace < "$patch_file"
+#    patch -p1 < "$patch_file"
 #  done
 #else
 #  echo "patching susfs failed, the kernel version you want to patch doesnt have patches here yet"
@@ -26,19 +26,13 @@ patchesdir="$outside/ksu/sukisu/hooks/"
 
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
-    patch -p1 --fuzz=3 --ignore-whitespace < "$patch_file"
+    patch -p1 < "$patch_file"
   done
 else
   echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
   exit 1
 fi
 
-#if [ -f "KernelSU/kernel/tools/inline_hook_check.mk" ]; then
-#  sed -i 's/\$(error/\$(warning/g' KernelSU/kernel/tools/inline_hook_check.mk
-#fi
-#find . -type f \( -name "Kbuild" -o -name "*.mk" -o -name "Makefile" \) -exec sed -i 's/\$(error/\$(warning/g' {} +
-#echo "[FIX] Absolutely all strict blocks downgraded to warnings!"
-
 sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-suki${KSU_ver}\"/" "${defconfig_file}"
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
-echo -e " \nIntegration ReSukiSU ver ${KSU_ver}" >> banner_append
+echo -e " \nReSukiSU Enable! resukisu ver ${KSU_ver}" >> banner_append
