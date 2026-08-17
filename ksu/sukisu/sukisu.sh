f2bfcb007fca48544f723d26d8d21449f22127b7
MrGadget84 <superhyper52@gmail.com>
2026-08-17 15:18:13 +0300

    Update sukisu.sh

--- ksu/sukisu/sukisu.sh
+++ ksu/sukisu/sukisu.sh
@@ -13,16 +13,16 @@ KSU_git_ver=$(cd $SUKI_DIR && git rev-list --count HEAD)
 KSU_ver=$KSU_git_ver
 
 patchesdir="$outside/ksu/sukisu/hooks/"
-suspatchesdir="$outside/ksu/sukisu/sus/"
+# suspatchesdir="$outside/ksu/sukisu/sus/"
 
-if [[ -d "$suspatchesdir" ]]; then
-  for patch_file in "$suspatchesdir"/*.patch ; do
-    patch -p1 --fuzz=3 --ignore-whitespace < "$patch_file"
-  done
-else
-  echo "patching susfs failed, the kernel version you want to patch doesnt have patches here yet"
-  exit 1
-fi
+#if [[ -d "$suspatchesdir" ]]; then
+#  for patch_file in "$suspatchesdir"/*.patch ; do
+#    patch -p1 --fuzz=3 --ignore-whitespace < "$patch_file"
+#  done
+#else
+#  echo "patching susfs failed, the kernel version you want to patch doesnt have patches here yet"
+#  exit 1
+#fi
 
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
@@ -33,11 +33,11 @@ else
   exit 1
 fi
 
-if [ -f "KernelSU/kernel/tools/inline_hook_check.mk" ]; then
-  sed -i 's/\$(error/\$(warning/g' KernelSU/kernel/tools/inline_hook_check.mk
-fi
-find . -type f \( -name "Kbuild" -o -name "*.mk" -o -name "Makefile" \) -exec sed -i 's/\$(error/\$(warning/g' {} +
-echo "[FIX] Absolutely all strict blocks downgraded to warnings!"
+#if [ -f "KernelSU/kernel/tools/inline_hook_check.mk" ]; then
+#  sed -i 's/\$(error/\$(warning/g' KernelSU/kernel/tools/inline_hook_check.mk
+#fi
+#find . -type f \( -name "Kbuild" -o -name "*.mk" -o -name "Makefile" \) -exec sed -i 's/\$(error/\$(warning/g' {} +
+#echo "[FIX] Absolutely all strict blocks downgraded to warnings!"
 
 sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-suki${KSU_ver}\"/" "${defconfig_file}"
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
