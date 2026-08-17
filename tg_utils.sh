1d6e7a724bfe5571cfb3942365d086a376b756b7
MrGadget84 <superhyper52@gmail.com>
2026-08-17 16:13:40 +0300

    Update tg_utils.sh

--- tg_utils.sh
+++ tg_utils.sh
@@ -6,14 +6,12 @@ case $1 in
   msg)
     curl -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
       -d chat_id="${CHAT_ID}" \
-      -d message_thread_id="7631" \
       -d "disable_web_page_preview=true" \
       -d "parse_mode=html" \
       -d text="$(echo "$2" | sed 's/%nl/\n/g')"
   ;;
   up | upload)
     curl -F chat_id="${CHAT_ID}" \
-      -F message_thread_id="7631"
       -F document=@"$2" \
       -F parse_mode=html https://api.telegram.org/bot${BOT_TOKEN}/sendDocument \
       -F caption="$(echo "$3" | sed 's/%nl/\n/g')"
