d5369129c68fb8ce1e9fcc14e135300092bb913c
MrGadget84 <superhyper52@gmail.com>
2026-08-18 13:16:55 +0400

    Update scripts for legacy and infinity branch

---
+++ ksu/ksu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# ReSukiSU (Non-SuSFS)
+
+export maindir="$(pwd)"
+export outside="${maindir}/.."
+source "${outside}/$1env"
+
+curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash
+git add . && git commit -am "drivers: KernelSU"
+SUKI_DIR="drivers/kernelsu"
+KSU_git_ver=$(cd $SUKI_DIR && git rev-list --count HEAD)
+KSU_ver=$KSU_git_ver
+
+patchesdir="$outside/ksu/hooks/"
+
+if [[ -d "$patchesdir" ]]; then
+  for patch_file in "$patchesdir"/*.patch ; do
+    patch -p1 < "$patch_file"
+  done
+else
+  echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
+  exit 1
+fi
+
+sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-${KSU_ver}\"/" "${defconfig_file}"
+echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
+echo -e " \nReSukiSU Enable! resukisu ver ${KSU_ver}" >> banner_append
--- ksu/susfs.sh
+++ ksu/susfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# UwU
+# ReSukiSU & SuSFS
 
 export maindir="$(pwd)"
 export outside="${maindir}/.."
@@ -12,17 +12,17 @@ SUKI_DIR="drivers/kernelsu"
 KSU_git_ver=$(cd $SUKI_DIR && git rev-list --count HEAD)
 KSU_ver=$KSU_git_ver
 
-patchesdir="$outside/ksu/sukisu/hooks/"
-#suspatchesdir="$outside/ksu/sukisu/sus/"
+patchesdir="$outside/ksu/hooks/"
+suspatchesdir="$outside/ksu/sus/"
 
-#if [[ -d "$suspatchesdir" ]]; then
-#  for patch_file in "$suspatchesdir"/*.patch ; do
-#    patch -p1 < "$patch_file"
-#  done
-#else
-#  echo "patching susfs failed, the kernel version you want to patch doesnt have patches here yet"
-#  exit 1
-#fi
+if [[ -d "$suspatchesdir" ]]; then
+  for patch_file in "$suspatchesdir"/*.patch ; do
+    patch -p1 < "$patch_file"
+  done
+else
+  echo "patching susfs failed, the kernel version you want to patch doesnt have patches here yet"
+  exit 1
+fi
 
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
@@ -33,6 +33,6 @@ else
   exit 1
 fi
 
-sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-suki${KSU_ver}\"/" "${defconfig_file}"
+sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-${KSU_ver}-susfs\"/" "${defconfig_file}"
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
 echo -e " \nReSukiSU Enable! resukisu ver ${KSU_ver}" >> banner_append
