ddc9b9ae530ea352dad539e99bec2ec14412552c
MrGadget84 <superhyper52@gmail.com>
2026-09-24 16:59:01 +0300

    Update ksu-next.sh

--- ksu/ksu-next/ksu-next.sh
+++ ksu/ksu-next/ksu-next.sh
@@ -6,7 +6,7 @@ export maindir="$(pwd)"
 export outside="${maindir}/.."
 source "${outside}/$1env"
 
-curl -LSs "https://raw.githubusercontent.com/White-Society/WhiteSU/main/kernel/setup.sh" | bash -s legacy_susfs
+curl -LSs "https://raw.githubusercontent.com/White-Society/WhiteSU/legacy/kernel/setup.sh" | bash -s legacy
 git add . && git commit -am "drivers: KernelSU"
 KSU_git_ver=$(cd WhiteSU && git rev-list --count HEAD)
 KSU_ver=$KSU_git_ver
