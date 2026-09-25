4b6ce6c02a24b4d4957973bb58f392e7da31fabd
MrGadget84 <superhyper52@gmail.com>
2026-09-25 19:51:58 +0400

    вась, че за дела вась, какой ксун вась

--- ksu/ksu-next/ksu-next.sh
+++ ksu/ksu-next/ksu-next.sh
@@ -11,7 +11,7 @@ git add . && git commit -am "drivers: KernelSU"
 KSU_git_ver=$(cd KernelSU-Next && git rev-list --count HEAD)
 KSU_ver=$KSU_git_ver
 
-patchesdir="$outside/ksu/ksu-next/patches/$(echo $kernel_ver | cut -d. -f1,2)"
+patchesdir="$outside/ksu/ksu-next/patches/"
 
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
