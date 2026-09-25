37f3c5eb58ccccebccad7927b88d2ed5eeb14cbb
MrGadget84 <superhyper52@gmail.com>
2026-09-25 22:00:06 +0400

    вась я устал вась

--- ksu/ksu-next/ksu-next.sh
+++ ksu/ksu-next/ksu-next.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #
-# white
+# Вась ну это уже позор вась
 
 export maindir="$(pwd)"
 export outside="${maindir}/.."
 source "${outside}/$1env"
 
-curl -LSs "https://raw.githubusercontent.com/White-Society/WhiteSU/main/kernel/setup.sh" | bash -s legacy
+curl -LSs "https://raw.githubusercontent.com/White-Society/WhiteSU/dev/kernel/setup.sh" | bash -s legacy
 git add . && git commit -am "drivers: KernelSU"
-KSU_git_ver=$(cd KernelSU-Next && git rev-list --count HEAD)
+KSU_git_ver=$(cd WhiteSU && git rev-list --count HEAD)
 KSU_ver=$KSU_git_ver
 
+echo 'CONFIG_KSU_EXTRAS=y' >> "${defconfig_file}"
 patchesdir="$outside/ksu/ksu-next/patches/"
-
 if [[ -d "$patchesdir" ]]; then
   for patch_file in "$patchesdir"/*.patch ; do
     git am "$patch_file"
@@ -22,32 +22,6 @@ else
   exit 1
 fi
 
-if ! grep -q "int path_umount" fs/namespace.c; then
-    cat <<EOF >> fs/namespace.c
-
-int path_umount(struct path *path, int flags)
-{
-    struct mount *mnt = real_mount(path->mnt);
-    int ret;
-
-    if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))
-        return -EINVAL;
-    if (!may_mount())
-        return -EPERM;
-    if (path->dentry != path->mnt->mnt_root)
-        return -EINVAL;
-
-    ret = do_umount(mnt, flags);
-
-    dput(path->dentry);
-    mntput_no_expire(mnt);
-    return ret;
-}
-EXPORT_SYMBOL(path_umount);
-EOF
-fi
-
-sed -i '/int do_umount(/a int path_umount(struct path *path, int flags);' include/linux/fs.h
-sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ksn${KSU_ver}\"/" "${defconfig_file}"
+sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ks${KSU_ver}\"/" "${defconfig_file}"
 echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
-echo -e " \nKernelSU-Next Version Enable, ksn ver ${KSU_ver}" >> banner_append
+echo -e " \nincludes White, ver ${KSU_ver}" >> banner_append
